d89145d5c96f01da6edcd6665439a8628b4a960b
Ethan Hasbrouck <ethanhasbrouck02@gmail.com>
2022-11-22 08:47:35 -0500

    Project setup

---
+++ components/landing/Splash.js
@@ -0,0 +1,3 @@
+export default function Splash() {
+    return <h1 className="flex justify-center text-7xl m-60">Splash</h1>;
+}
---
+++ components/landing/Title.js
@@ -0,0 +1,7 @@
+export default function Title() {
+    return (
+        <h1 className="flex justify-start font-extrabold text-3xl m-7">
+            Product Title
+        </h1>
+    );
+}
